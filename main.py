df8e7a2a2866cf7b30683cbee39b87fc9d1ea73d
Frank 'The Tank' Nardone <frank.nardone@nfrastructure.com>
2019-04-12 10:18:25 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 13:19:21 2019
+
+@author: jjy
+"""
+
+from scr.roster import updateroster #gets roster from yahoo api
+
+updateroster(leagueid=59224,numteams=12)
